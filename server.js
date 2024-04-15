b6bb5359c458ec4905323a8ff407a4a29f7f4f82
Manikm07-nikk <manikmukhija6666@gmail.com>
2024-04-15 15:07:36 +0530

    added delete

--- server.js
+++ server.js
@@ -51,6 +51,90 @@ app.post('/api/dns/create', async (req, res) => {
   }
 });
 
+app.get('/api/dns', async (req, res) => {
+  try {
+    // Retrieve all hosted zones
+    const { HostedZones } = await route53.listHostedZones().promise();
+
+    // Array to store DNS records
+    const dnsRecords = [];
+
+    // Iterate through hosted zones to fetch records
+    for (const hostedZone of HostedZones) {
+      // Retrieve DNS records for the hosted zone
+      const { ResourceRecordSets } = await route53.listResourceRecordSets({
+        HostedZoneId: hostedZone.Id,
+      }).promise();
+
+      // Push each record to the dnsRecords array
+      for (const record of ResourceRecordSets) {
+        if (record.Type === 'SOA' || record.Type === 'NS') {
+          continue; // Skip this iteration and move to the next one
+        }
+        dnsRecords.push({
+          hostedZoneId: hostedZone.Id,
+          domain: record.Name,
+          type: record.Type,
+          value: record.ResourceRecords.map(record => record.Value).join(', '), // Concatenate multiple values if present
+        });
+      }
+    }
+
+    console.log('DNS records fetched successfully');
+    res.json(dnsRecords);
+  } catch (err) {
+    console.error('Error fetching DNS records:', err.stack); // Log the error stack trace
+    res.status(500).json({ error: 'Failed to fetch DNS records', details: err.message });
+  }
+});
+
+
+
+app.delete('/api/dns/delete/:domain', async (req, res) => {
+  const { domain } = req.params;
+  let { type, values } = req.query;
+  if(values.includes(',')){
+    values = values.split(',').join('", "');
+  }
+
+  try {
+    let hostedZone = await checkHostedZone(domain);
+
+    if (!hostedZone) {
+      res.json("Hosted Zone not found");
+      return; // Exit the function if hosted zone not found
+    }
+
+    
+
+    // console.log(resourceRecords);
+    const params = {
+      HostedZoneId: hostedZone.Id,
+      ChangeBatch: {
+        Changes: [
+          {
+            Action: 'DELETE',
+            ResourceRecordSet: {
+              Name: domain,
+              Type: type,
+              TTL: 300,
+              ResourceRecords: [{Value : values.trim()}],
+            },
+          },
+        ],
+      },
+    };
+
+    await route53.changeResourceRecordSets(params).promise();
+
+    console.log('DNS record deleted successfully');
+    res.json({ message: 'DNS record deleted successfully' });
+  } catch (err) {
+    console.error('Error deleting DNS record:', err.stack); // Log the error stack trace
+    res.status(500).json({ error: 'Failed to delete DNS record', details: err.message });
+  }
+});
+
 async function checkHostedZone(domain) {
   const { HostedZones } = await route53.listHostedZonesByName({ DNSName: domain }).promise();
   return HostedZones.find(zone => zone.Name === domain);
