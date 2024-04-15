9a1f991b2a3dd214b8bbf5e9168a213f0283a138
Manikm07-nikk <110350100+Manikm07-nikk@users.noreply.github.com>
2024-04-15 16:17:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,19 @@ const AWS = require('aws-sdk');
 const app = express();
 
 // Enable CORS for all routes
-app.use(cors({
-  origin: 'https://dns-manager-nine.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-app.options('*', cors());
+// app.use(cors({
+//   origin: 'https://dns-manager-nine.vercel.app',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// }));
+// app.options('*', cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://dns-manager-nine.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 
 //app.use(cors());
